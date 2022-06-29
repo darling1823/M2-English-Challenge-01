4e0af57e0845c9fe01ad99e5401899d99b72230a
darling1823 <gabrielalcantara1823@gmail.com>
2022-06-28 21:03:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+let comedy = [{
+    title : 'Red: Crescer é uma fera',
+    director : 'Domee Shi',
+    year : 2022,
+},{
+    title : 'Mamma Mia!',
+    director : 'Phyllida Lloyd',
+    year : 2008,
+},{
+    title : 'Minions',
+    director : 'Pierre Coffin, Kyle Balda',
+    year : 2015,
+}]
+let action = [{
+    title : 'Doutor Estranho no Multiverso da Loucura',
+    director : 'Sam Reimi',
+    year : 2022,
+},{
+    title : 'Onde está Segunda?',
+    director : 'Tommy Wirkola',
+    year : 2017,
+},{
+    title : 'Venom',
+    director : 'Ruben Fleischer',
+    year : 2018,
+}
+]
+let fantasy = [{
+    title : 'A Origem dos Guardiões',
+    director : 'Peter Ramsey',
+    year : 2012,
+},{
+    title : 'A Princesa e o Sapo',
+    director : 'John Musker, Ron Clements',
+    year : 2009,
+},{
+    title : 'Bolt',
+    director : 'Byron Howard, Chris Williams',
+    year : 2009,
+}]
+
+if(fantasy.index = [1]){
+    console.log(`My favorite movie is ${fantasy[1].title}`)
+}else{
+    console.log('Not even 5 stars...')
+}
